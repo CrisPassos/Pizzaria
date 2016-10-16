1ea789a6c45e87fd2e0f90a1e4c8623b1dc5e570
Cristiana <cristiana.pazos@gmail.com>
2016-10-16 14:12:18 -0200

    Initial commit

---
+++ app/src/main/java/br/com/cristiana/pizzaria/PizzariaActivity.java
@@ -0,0 +1,13 @@
+package br.com.cristiana.pizzaria;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PizzariaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pizzaria);
+    }
+}
