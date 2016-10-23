e9c9320cf799bf5e6639dac892cb0e05a6813395
Cristiana <cristiana.pazos@gmail.com>
2016-10-23 14:31:40 -0200

    Acrescentado o Spinner

--- app/src/main/java/br/com/cristiana/pizzaria/PizzariaActivity.java
+++ app/src/main/java/br/com/cristiana/pizzaria/PizzariaActivity.java
@@ -5,12 +5,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 public class PizzariaActivity extends AppCompatActivity {
 
     RadioGroup rdgSabores;
     CheckBox ckbBorda;
+    Spinner spFormaPagamento;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +21,7 @@ public class PizzariaActivity extends AppCompatActivity {
 
         rdgSabores = (RadioGroup) findViewById(R.id.rbgSabores);
         ckbBorda = (CheckBox) findViewById(R.id.ckbBorda);
+        spFormaPagamento = (Spinner) findViewById(R.id.spFormaPagamento);
 
         //O evento ChangeListener verifica toda vez que for clicado o check
         rdgSabores.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
@@ -40,6 +43,9 @@ public class PizzariaActivity extends AppCompatActivity {
     public void calcular(View v){
 
         int saborId = rdgSabores.getCheckedRadioButtonId();
+        int valorSelecionado = spFormaPagamento.getSelectedItemPosition();
+
+        String texto = spFormaPagamento.getSelectedItem().toString();
 
         double valor;
         if (saborId == R.id.rbt_queijo){
@@ -54,7 +60,7 @@ public class PizzariaActivity extends AppCompatActivity {
             valor += 5.0;
         }
 
-        Toast msg = Toast.makeText(this, getString(R.string.app_valor) + valor, Toast.LENGTH_SHORT);
+        Toast msg = Toast.makeText(this, getString(R.string.app_valor) + " " + valor + " "+ texto, Toast.LENGTH_SHORT);
         msg.show();
     }
 }
